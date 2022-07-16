6b1615a329ad6f264b6968c55c3cf1a0dc70fac0
Andrea Ceci <andrea@MacBook-Pro-di-Andrea.local>
2022-07-16 02:32:44 +0200

    Import del file

---
+++ Esame.py
@@ -0,0 +1,33 @@
+# Import CSV file and extract the values from file
+file = open('data.csv', 'r')
+
+values = []
+
+for line  in file:
+
+    values.append(line)
+
+time = []
+value = []
+
+# Check if the CSV file has a str header
+
+if values[0].split(',')[0] == 'epoch':
+    for line in values[1:len(values)]:
+        x = line.split(',')
+        time.append(x[0])
+        value.append(x[1])
+else:
+        for line in values:
+            x = line.split(',')
+            time.append(x[0])
+            value.append(x[1])
+
+file.close() 
+
+print(time[0])
+
+
+
+
+
